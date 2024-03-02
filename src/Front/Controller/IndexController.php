9291584f46145dc89c9b8cff92372aa44ac6640e
MCarballo <m.carballo.temp@gmail.com>
2024-03-02 11:53:11 +0100

    first front controller

---
+++ src/Front/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Front\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: ' . $number . '</body></html>'
+        );
+    }
+}
