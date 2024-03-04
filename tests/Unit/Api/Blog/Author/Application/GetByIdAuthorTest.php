ab2f6f16cc876eab4b6c1201fa12aaeae9b27b0f
MCarballo <m.carballo.temp@gmail.com>
2024-03-04 08:57:19 +0100

    faker

--- tests/Unit/Api/Blog/Author/Application/GetByIdAuthorTest.php
+++ tests/Unit/Api/Blog/Author/Application/GetByIdAuthorTest.php
@@ -6,6 +6,7 @@ use App\Api\Blog\Author\Application\DTO\AuthorDTO;
 use App\Api\Blog\Author\Application\GetByIdAuthor;
 use App\Api\Blog\Author\Domain\Author;
 use App\Api\Blog\Author\Domain\AuthorRepository;
+use App\Tests\Utilities\Faker;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
@@ -37,20 +38,20 @@ class GetByIdAuthorTest extends TestCase
         return [
             [
                 Author::fromPrimitives(
-                    1,
-                    'Leanne Graham',
-                    'Sincere@april.biz',
-                    'Kulas Light',
-                    'Apt. 556',
-                    'Gwenborough',
-                    '92998-3874',
-                    '-37.3159',
-                    '81.1496',
-                    '1-770-736-8031 x56442',
-                    'hildegard.org',
-                    'Romaguera-Crona',
-                    'Multi-layered client-server neural-net',
-                    'harness real-time e-markets'
+                    3,
+                    Faker::get()->title(),
+                    Faker::get()->email(),
+                    Faker::get()->streetAddress(),
+                    Faker::get()->streetAddress(),
+                    Faker::get()->city(),
+                    Faker::get()->postcode(),
+                    Faker::get()->latitude(-90, 90),
+                    Faker::get()->longitude(-180, 180),
+                    Faker::get()->phoneNumber(),
+                    Faker::get()->domainName(),
+                    Faker::get()->title(),
+                    Faker::get()->catchPhrase(),
+                    Faker::get()->bs()
                 ),
             ],
         ];
