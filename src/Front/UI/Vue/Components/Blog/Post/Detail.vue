698d6a072c2f60ce30233c1889736c6df89e674c
MCarballo <m.carballo.temp@gmail.com>
2024-03-05 19:14:40 +0100

    make-up

--- src/Front/UI/Vue/Components/Blog/Post/Detail.vue
+++ src/Front/UI/Vue/Components/Blog/Post/Detail.vue
@@ -1,29 +1,37 @@
 <template>
-  <div class="container">
-    <h1>{{ post.title }}</h1>
-    <p>{{ post.body }}</p>
-    <div class="card">
-      <div class="card-body">
-        <h3>{{ post.author.name }}</h3>
-        <div class="author-details">
-          <p>Email: {{ post.author.email }}</p>
-          <p>
-            Address: {{ post.author.address.street }}, {{ post.author.address.suite }},
-            {{ post.author.address.city }}, {{ post.author.address.zipcode }}
-          </p>
-          <p>Phone: {{ post.author.phone }}</p>
-          <p>
-            Website:
-            <a :href="`http://${post.author.website}`" target="_blank">{{
-              post.author.website
-            }}</a>
-          </p>
-          <p>Company: {{ post.author.company.name }}</p>
+  <div class="container my-5">
+    <div class="mb-4">
+      <router-link to="/" class="btn btn-primary">← Back to Posts</router-link>
+    </div>
+    <div class="row">
+      <div class="col-lg-8">
+        <article class="mb-3">
+          <h1 class="mb-3">{{ post.title }}</h1>
+          <div class="post-body">
+            <p>{{ post.body }}</p>
+          </div>
+        </article>
+      </div>
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-header">
+            Author Info
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><strong>Name:</strong> {{ post.author.name }}</li>
+            <li class="list-group-item"><strong>Email:</strong> <a :href="`mailto:${post.author.email}`">{{ post.author.email }}</a></li>
+            <li class="list-group-item">
+              <strong>Address:</strong> {{ post.author.address.street }}, {{ post.author.address.suite }},
+              {{ post.author.address.city }}, {{ post.author.address.zipcode }}
+            </li>
+            <li class="list-group-item"><strong>Phone:</strong> {{ post.author.phone }}</li>
+            <li class="list-group-item">
+              <strong>Website:</strong> <a :href="`http://${post.author.website}`" target="_blank">{{ post.author.website }}</a>
+            </li>
+            <li class="list-group-item"><strong>Company:</strong> {{ post.author.company.name }}</li>
+          </ul>
         </div>
       </div>
-    </div>
-    <div class="mt-3">
-      <router-link to="/" class="btn btn-primary">Back</router-link>
     </div>
   </div>
 </template>
--- src/Front/UI/Vue/Components/Blog/Post/ListAll.vue
+++ src/Front/UI/Vue/Components/Blog/Post/ListAll.vue
@@ -1,28 +1,23 @@
 <template>
-  <div class="container">
-    <h1>Posts</h1>
-    <div class="table-responsive">
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="post in posts" :key="post.id">
-            <td>
-              <router-link :to="{ name: 'PostDetail', params: { id: post.id }}">{{ post.title }}</router-link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+  <div class="container my-1">
+    <h1 class="mb-4">Posts</h1>
+    <div class="row">
+      <div class="col-md-4 d-flex align-items-stretch" v-for="post in posts" :key="post.id">
+        <div class="card mb-4 w-100">
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title mb-2">{{ post.title }}</h5>
+            <div class="mt-auto">
+              <router-link :to="{ name: 'PostDetail', params: { id: post.id }}" class="btn btn-primary">Read</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
@@ -34,18 +29,18 @@ export default {
     this.loadPosts();
   },
   watch: {
-    '$route'(to, from) {
-      // Llamada para cargar los datos cuando cambia la ruta
+    $route(to, from) {
       this.loadPosts();
-    }
+    },
   },
   methods: {
     loadPosts() {
-      axios.get('/api/blog/posts')
-        .then(response => {
+      axios
+        .get("/api/blog/posts")
+        .then((response) => {
           this.posts = response.data;
         })
-        .catch(error => console.error("Error loading Posts data", error));
+        .catch((error) => console.error("Error loading Posts data", error));
     },
   },
 };
