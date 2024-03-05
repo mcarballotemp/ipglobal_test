0b0cbeb6a09eeacd1d1c7b1b8738541d257e8978
MCarballo <m.carballo.temp@gmail.com>
2024-03-05 14:24:08 +0100

    Vue

---
+++ assets/vue/App.vue
@@ -0,0 +1,27 @@
+// assets/vue/App.vue
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hola desde Vue!",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
